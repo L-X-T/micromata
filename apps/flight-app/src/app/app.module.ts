7be9c85abdddac45c62e1b9f24bac487dbb6ccea
Alex Thalhammer <alex@angular-software.at>
2021-08-16 18:15:18 +0200

    [RxJs] Simple Lookahead

--- apps/flight-app/src/app/app.module.ts
+++ apps/flight-app/src/app/app.module.ts
@@ -14,12 +14,15 @@ import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SharedModule } from './shared/shared.module';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FlightLookaheadComponent } from './flight-lookahead/flight-lookahead.component';
 
 @NgModule({
   imports: [
     BrowserModule,
     HttpClientModule,
     FlightBookingModule,
+    ReactiveFormsModule,
 
     BrowserAnimationsModule,
     FlightCancellingModule,
@@ -28,7 +31,7 @@ import { SidebarComponent } from './sidebar/sidebar.component';
     SharedModule.forRoot(),
     RouterModule.forRoot(APP_ROUTES, { relativeLinkResolution: 'legacy' })
   ],
-  declarations: [AppComponent, SidebarComponent, NavbarComponent, HomeComponent, BasketComponent],
+  declarations: [AppComponent, SidebarComponent, NavbarComponent, HomeComponent, BasketComponent, FlightLookaheadComponent],
   providers: [],
   bootstrap: [AppComponent]
 })
